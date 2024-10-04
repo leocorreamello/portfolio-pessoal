b2ca07f3d7093cd7dd9eb65cd0ac7f14e8d38162
leocorreamello <leo.correamello@gmail.com>
2024-10-04 11:26:04 -0300

    Menu Responsivo

--- portifolio/src/components/Header.jsx
+++ portifolio/src/components/Header.jsx
@@ -1,34 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 import logo from "../../public/logo.png";
+import menu from "../../public/iconmenu.png";
 
 export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <>
-            <header className="bg-black flex text-white justify-around h-20 items-center text-2xl">
+            <header className="bg-black fixed top-0 w-full flex text-white justify-between h-20 items-center text-2xl px-4 z-50">
                 <div>
                     <img className="w-1/4" src={logo} alt="logo" />
                 </div>
-                <nav>
-                    <ul className="xl:text-3xl lg:text-3xl md:text-2xl sm:text-2xl ssm-text-1xl flex space-x-10">
+                <nav className="hidden lg:flex space-x-4">
+                    <ul className="flex space-x-4">
                         <li>
                             <Link to="home" smooth={true} duration={500}>
-                            Principal
+                                Principal
                             </Link>
                         </li>
                         <li>
                             <Link to="sobre" smooth={true} duration={500}>
-                            Sobre
+                                Sobre
                             </Link>
                         </li>
                         <li>
                             <Link to="projetos" smooth={true} duration={500}>
-                            Projetos
+                                Projetos
                             </Link>
                         </li>
                     </ul>
                 </nav>
+                <div className="lg:hidden">
+                    <button onClick={toggleMenu}>
+                        <img src={menu} alt="icon-menu" className="w-10" />
+                    </button>
+                </div>
             </header>
+            {isMenuOpen && (
+                <div className="lg:hidden bg-black text-white fixed top-20 w-full z-40">
+                    <ul className="flex flex-col space-y-4 p-4">
+                        <li>
+                            <Link to="home" smooth={true} duration={500} onClick={toggleMenu}>
+                                Principal
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="sobre" smooth={true} duration={500} onClick={toggleMenu}>
+                                Sobre
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="projetos" smooth={true} duration={500} onClick={toggleMenu}>
+                                Projetos
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            )}
         </>
-    )
+    );
 }
